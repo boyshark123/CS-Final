fcc7d23b7c9355a1ee26bd242181f61553681d3f
MargaretThatcherLikesJava <75631117+MargaretThatcherLikesJava@users.noreply.github.com>
2020-12-14 21:44:46 -0500

    Conditional fixed

--- hashmap.java
+++ hashmap.java
@@ -11,30 +11,30 @@ public class hashmap {
 
         // List the rooms
         places.put("start", "Zoo Entrance.");
-        places.put("exhibit 1", "Baboon.");
-        places.put("exhibit 2", "Rhino.");
-        places.put("exhibit 3", "Lion.");
-        places.put("exhibit 4", "Elephant.");
+        places.put("exhibit1", "Baboon.");
+        places.put("exhibit2", "Rhino.");
+        places.put("exhibit3", "Lion.");
+        places.put("exhibit4", "Elephant.");
         places.put("end", "End of the Zoo");
                 
         // Establish moves
-        String[] startMoves = {"start", "exhibit 1"}; 
+        String[] startMoves = {"start", "exhibit1"}; 
         moves.put("start", startMoves);
      
         // Chose exhibit 1
-        String[] room1moves = {"start", "exhibit 2"}; 
+        String[] room1moves = {"start", "exhibit2"}; 
         moves.put("exhibit 1", room1moves);
         
         // Chose exhibit 2
-        String[] room2moves = {"exhibit 1", "exhibit 3"}; 
+        String[] room2moves = {"exhibit1", "exhibit3"}; 
         moves.put("exhibit 2", room2moves);
         
         // Chose exhibit 3
-        String[] room3moves = {"exhibit 2", "exhibit 4"}; 
+        String[] room3moves = {"exhibit2", "exhibit4"}; 
         moves.put("exhibit 3", room3moves);
         
         // Chose exhibit 4
-        String[] room4moves = {"exhibit 3", "end"}; 
+        String[] room4moves = {"exhibit3", "end"}; 
         moves.put("exhibit 4", room4moves);
 
         // Starting of zoo
@@ -50,10 +50,8 @@ public class hashmap {
             System.out.println("LOCATION: " + currentRoom);
             System.out.println("What room will you enter?");
 
-            
-
             // Display room options
-            for(int i = 0; i < nextSteps.length; i++){
+            for (int i = 0; i < nextSteps.length; i++){
                 System.out.println("- " + nextSteps[i]);
             }
 
@@ -62,16 +60,18 @@ public class hashmap {
             System.out.println("Where do you wish to go?");
             currentRoom = currentRoomLocation.nextLine();
 
-            if (currentRoomLocation.equals(“start”){
+            if (currentRoom.equals("start")){
                 System.out.println();
-            } else if(currentRoomLocation.equals(“exhibit 1”){
+            } else if(currentRoom.equals("exhibit1")){
                 System.out.println();
-            } else if(currentRoomLocation.equals(“exhibit 2”){
+            } else if(currentRoom.equals("exhibit2")){
                 System.out.println();
-            } else if(currentRoomLocation.equals(“exhibit 3”){
+            } else if(currentRoom.equals("exhibit3")){
                 System.out.println();
-            } else if(currentRoomLocation.equals(“exhibit 4”){
+            } else if(currentRoom.equals("exhibit4")){
                 System.out.println();
+            } else {
+                System.out.println("No information on this exhibit.");
             }
 
             // New line for organziation
